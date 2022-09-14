4f699ca8901e2d2ae914951a14b4c5f567d0ff5c
enrique-zarate <eszg.fury@gmail.com>
2022-09-13 23:13:08 -0400

    funciones, pseudocodigo

---
+++ carrito_ejemplo.js
@@ -0,0 +1,10 @@
+let carrito = {
+    productos: ['correa', 'shampoo'],
+    precios: [5000, 6000]
+
+}
+function calcularTotal (carrito) {
+        for p in carrito.precio:
+            total = total + p
+}
+
--- scripts.js
+++ scripts.js
@@ -1,4 +1,23 @@
-function miFuncion() {
+function anhadirCarrito() {
+  // obtener el precio del producto
   let price = document.getElementById("price").innerText;
   console.log(price);
+
+  // obtener el nombre del producto
+
+  // anhadir datos al objeto carrito
+}
+
+function calcularTotal() {
+  // Bucle para desplegar info de la compra: nombre productos y precios
+  // Validar que el carrito no este vacio. Si esta vacio: alert(),
+  // sino: sumar precios recorriendo los precios del objeto Carrito, calcular IVA y deplegar subtotal total
+  // Validar datos de delivery: Nombre y apellido, direccion y edad no esten vacios. Sumar al monto total
+  // Restar % cupon de descuento al subtotal -> agregar en HTML un campo para poner cupon descuento
+  // En caso de que aun tenga plata en la Billetera, descontar, si no, avisar insuficiencia de fondos
+}
+
+function descontarBilletera() {
+  // tomar el monto total y restar al saldo
+  // desplegar en pantalla
 }
